c29c3f0aa4fc7d486d842cf73b7e313ee907a7e8
Soubhik Kumar Gon <b422056@iiit-bh.ac.in>
2023-06-11 10:59:43 +0530

    scraping names off sites

--- chrome-automation-puppeteer/index.js
+++ chrome-automation-puppeteer/index.js
@@ -1,13 +1,41 @@
 const puppeteer = require('puppeteer')
-
+const fs = require('fs/promises')//sing promises version of fs so to avoid the messy callbacks and use benefits of promises
 /*Puppeteer works on promise based function.hence use asynchronous functions!*/
 
-async function takeScreenshot(){
+async function takeScreenshot() {
     const browser = await puppeteer.launch()
     const page = await browser.newPage()
     await page.goto("https://en.wikipedia.org/wiki/JavaScript");
-    await page.screenshot({path:"wikipedia.png",fullPage:true});
+    await page.screenshot({ path: "wikipedia.png", fullPage: true });
     await browser.close()
 }
 
-takeScreenshot();
+// takeScreenshot();
+
+async function scrapeNames() {
+    const browser = await puppeteer.launch()
+    const page = await browser.newPage()
+    await page.goto("https://learnwebcode.github.io/practice-requests/");
+    //div.info > strong
+    const names = await page.evaluate(() => {
+        return Array.from(document.querySelectorAll(".info strong")).map(x => {
+            return x.textContent
+        })
+    })
+//used for selecting multiple images
+//Q.what is nodelist? 
+    const photos = page.$$eval("img",(imgs)=>{
+        return imgs.map(x=>x.src)
+    }) 
+
+    await fs.writeFile("cats.txt", names.join("\n"));
+
+
+
+
+    // const names = ['red','orange','yellow'];
+    // await fs.writeFile("names.txt",names.join("\n"));
+    await browser.close()
+
+}
+scrapeNames();
