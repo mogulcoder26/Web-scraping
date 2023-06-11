35875658200c141e5ebc07734cad3bf921b3b72f
Soubhik Kumar Gon <b422056@iiit-bh.ac.in>
2023-06-11 13:12:08 +0530

    added cron jobs and submitting forms method

--- chrome-automation-puppeteer/index.js
+++ chrome-automation-puppeteer/index.js
@@ -1,6 +1,9 @@
 const puppeteer = require('puppeteer')
-const fs = require('fs/promises')//sing promises version of fs so to avoid the messy callbacks and use benefits of promises
+const fs = require('fs/promises');//sing promises version of fs so to avoid the messy callbacks and use benefits of promises
+const { it } = require('node:test');
 /*Puppeteer works on promise based function.hence use asynchronous functions!*/
+
+const cron = require("node-cron");
 
 async function takeScreenshot() {
     const browser = await puppeteer.launch()
@@ -24,9 +27,14 @@ async function scrapeNames() {
     })
 //used for selecting multiple images
 //Q.what is nodelist? 
-    const photos = page.$$eval("img",(imgs)=>{
+    const photos =await page.$$eval("img",(imgs)=>{
         return imgs.map(x=>x.src)
     }) 
+
+    for(const photo of photos){
+        const imagepage= await page.goto(photo)
+        await fs.writeFile(photo.split("/").pop(),await imagepage.buffer())
+    }
 
     await fs.writeFile("cats.txt", names.join("\n"));
 
@@ -38,4 +46,53 @@ async function scrapeNames() {
     await browser.close()
 
 }
-scrapeNames();
+// scrapeNames();
+
+// simulating clicking a button 
+
+async function clickSimulation(){
+    const browser = await puppeteer.launch()
+    const page = await browser.newPage()
+
+    await page.goto("https://learnwebcode.github.io/practice-requests/");
+
+
+    await page.click("#clickme");
+    const clickedData = await page.$eval("#data",element=>element.textContent)
+    console.log(clickedData)
+
+    browser.close();
+}
+
+// clickSimulation();
+
+
+//simulating submitting a form
+
+async function submitFormAndGetData(){
+    const  browser = await puppeteer.launch()
+    const page = await browser.newPage()
+
+    await page.goto("https://learnwebcode.github.io/practice-requests/");
+
+
+    await page.type("#ourfield","blue")
+    
+    await Promise.all([await page.click("#ourform button"),
+    await page.waitForNavigation()])
+
+    const info = await page.$eval("#message",el=>el.textContent)
+
+    console.log(info);
+    fs.writeFile("secret.txt",info)
+    browser.close()
+}
+
+// submitFormAndGetData()
+
+// scheduling the task and automating it
+// setInterval(submitFormAndGetData,5000);
+
+// But for automating it as like : 5th day of every week , etc :
+
+cron.schedule("*/5*****",submitFormAndGetData)
